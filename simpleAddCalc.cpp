c9bda4fc55e536ef822ea4e1b52c1e8d754fd7c7
DejamoAngel <marieangeldejamo@gmail.com>
2024-01-12 21:13:00 +0800

    second commit

---
+++ simpleAddCalc.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int a,b;
+    int sum;
+    cout << "Enter First Number: ";
+    cin >> a;
+    cout << "Enter Second Number: ";
+    cin >> b;
+    sum = a+b;
+    cout << "The Sum is: " << sum;
+    return 0;
+}
---
+++ simpleAddcalculator.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int a,b;
+    int sum;
+    cout << "Enter First Number: ";
+    cin >> a;
+    cout << "Enter Second Number: ";
+    cin >> b;
+    sum = a+b;
+    cout << "The Sum is: " << sum;
+    return 0;
+}
+
